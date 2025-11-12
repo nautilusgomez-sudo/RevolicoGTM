546956b94b9aea7005698882cf7b65081370babe
alvanta <nautilusgomez@gmail.com>
2025-11-12 11:14:18 -0500

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 const CONFIG = {
     GIST_ID: '0fa357699d922bb270bccf7e5edceb1d',  // ID del Gist para database.json
     GIST_FILE: 'database.json',  // Nombre del archivo en el Gist
-    POLL_INTERVAL: 30000,        // Intervalo de polling en ms (30 segundos)
+    POLL_INTERVAL: 10000,        // Intervalo de polling en ms (30 segundos)
     ADMIN_USER: 'admin',         // Usuario master inicial
     ADMIN_PASS_INICIAL: 'admin123'  // Contraseña inicial (se guarda hasheada en DB)
 };
